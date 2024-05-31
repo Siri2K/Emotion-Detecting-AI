d00860b1e26340fbbf5ad8c1e55e0b7e2a546842
Siri2K <kanagalingham.sriram@gmail.com>
2024-05-31 16:49:10 -0400

    Removed Dummy Code

--- src/data.py
+++ src/data.py
@@ -97,8 +97,6 @@ class EmotionImages:
         savedFileList: List[List[str]] = []
         imageList: List[List[Image.Image]] = []
         for root, directory, files in os.walk(desiredDirectory):
-            if not root.endswith("Dummy"):
-                continue
             savedFile: List[str] = []
             images: List[Image.Image] = []
             for file in files:
