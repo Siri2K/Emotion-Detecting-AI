c9ca2f1264b323e8fbb6d5fbb2b6c28ff254d1b7
Theebika6 <82008678+Theebika6@users.noreply.github.com>
2024-06-26 16:49:38 -0400

    added images for senior female and male

--- src/main.py
+++ src/main.py
@@ -82,10 +82,23 @@ def main():
         """
 
         # Train for Biasing
-        """
+
         trainCNN(dataLoader=[maleTrainDataloader, maleTesDataloader, maleValidationDataloader], model=model,
                  device=device, savePath=os.path.join(genderDataset.getDataDirectory(), "bin", saveFile))
-        """
+
+        trainCNN(dataLoader=[femaleTrainDataloader, femaleTesDataloader, femaleValidationDataloader], model=model,
+                 device=device, savePath=os.path.join(genderDataset.getDataDirectory(), "bin", saveFile))
+
+        trainCNN(dataLoader=[youngTrainDataloader, youngTesDataloader, youngValidationDataloader], model=model,
+                 device=device, savePath=os.path.join(ageDataset.getDataDirectory(), "bin", saveFile))
+
+        trainCNN(dataLoader=[middleTrainDataloader, middleTesDataloader, middleValidationDataloader], model=model,
+                 device=device, savePath=os.path.join(ageDataset.getDataDirectory(), "bin", saveFile))
+
+        trainCNN(dataLoader=[seniorTrainDataloader, seniorTesDataloader, seniorValidationDataloader], model=model,
+                 device=device, savePath=os.path.join(ageDataset.getDataDirectory(), "bin", saveFile))
+
+
 
 
         # Train & Test CNN Model with K-Fold
